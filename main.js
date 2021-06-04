831759f01c5d048192b0067701f5b936337533b0
Cptn Reef <sharif.staffordbz@gmail.com>
2021-06-04 14:32:14 -0400

    4th commit

--- main.js
+++ main.js
@@ -5,35 +5,74 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = -3;
 var dy = -1;
-var cr = 130;
-var cg = 105;
-var cb = 100;
 
 var ballRadius = 10;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
-    var ballColor = `rgb(${cr}, ${cg }, ${cb})`;
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = ballColor;
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();   
+    drawBall();
+    drawPaddle();
+    
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        cr -= 50;
-        cg -= 50;
-        cb -= 50;
         dx = -dx;
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        cr += 50;
-        cg += 50;
-        cb += 50;
         dy = -dy;
+    }
+    
+    if(rightPressed) {
+        paddleX += 12;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 12;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
     }
     
     x += dx;
