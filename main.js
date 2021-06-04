0b61d4dd690aac2326ff436628c4a711cb12edde
Cptn Reef <sharif.staffordbz@gmail.com>
2021-06-04 15:29:56 -0400

    6th commit

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
-
-var dx = -3;
-var dy = -1;
-
-var ballRadius = 10;
-
+var dx = 2;
+var dy = -2;
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 2;
+var brickColumnCount = 3;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -50,9 +62,25 @@ function drawPaddle() {
     ctx.fill();
     ctx.closePath();
 }
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
     
@@ -64,8 +92,9 @@ function draw() {
     }
     else if(y + dy > canvas.height-ballRadius) {
         if(x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
-            dy -= 3
+           if(y= y-paddleHeight){
+            dy = -dy  ;
+			 }
         }
         else {
             alert("GAME OVER");
@@ -84,4 +113,5 @@ function draw() {
     x += dx;
     y += dy;
 }
-setInterval(draw, 10);
+
+var interval = setInterval(draw, 10);
