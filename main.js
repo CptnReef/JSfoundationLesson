8ac35155c05dbaef05c5cda587ca6a63215ca080
Cptn Reef <sharif.staffordbz@gmail.com>
2021-06-04 14:26:36 -0400

    3rd commit

--- main.js
+++ main.js
@@ -5,21 +5,39 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = -3;
 var dy = -1;
+var cr = 130;
+var cg = 105;
+var cb = 100;
+
+var ballRadius = 10;
 
 function drawBall() {
-    // drawing code
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    var ballColor = `rgb(${cr}, ${cg }, ${cb})`;
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = ballColor;
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
+    drawBall();   
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        cr -= 50;
+        cg -= 50;
+        cb -= 50;
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        cr += 50;
+        cg += 50;
+        cb += 50;
+        dy = -dy;
+    }
+    
     x += dx;
     y += dy;
-
 }
+
 setInterval(draw, 10);
